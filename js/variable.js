4f3cf7d75915f3b8cfc438bac02cd0fdb6ec948a
youngeunkk <duddms0939@gmail.com>
2022-04-18 15:48:05 +0900

    variable.js

---
+++ js/variable.js
@@ -0,0 +1,19 @@
+let message;
+message = 'Hello'; // 문자열을 저장합니다.
+alert(message); // Hello!
+
+let userName;
+let test123;
+
+const myBirthday = '10.05.1996';
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+// 색상을 고르고 싶을 때 별칭을 사용할 수 있게 되었습니다.
+let color = COLOR_ORANGE;
+alert(color); // #FF7F00
+
+
