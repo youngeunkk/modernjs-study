af9568a9fd8c897e74be462a3235614f771cf1ac
youngeunkk <duddms0939@gmail.com>
2022-04-18 22:08:08 +0900

    variable 수정

--- js/variable/variable.js
+++ js/variable/variable.js
@@ -13,7 +13,8 @@ const COLOR_BLUE = "#00F";
 const COLOR_ORANGE = "#FF7F00";
 
 // 색상을 고르고 싶을 때 별칭을 사용할 수 있게 되었습니다.
+
 let color = COLOR_ORANGE;
-alert(color); // #FF7F00
+alert(color); // #FF7F00 
 
 
